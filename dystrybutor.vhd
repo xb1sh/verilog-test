3039e161ac82a89dc40cfa4ea697933d00033719
xb1sh <129178652+xb1sh@users.noreply.github.com>
2023-03-28 14:07:30 +0200

    Add files via upload

---
+++ dystrybutor.vhd
@@ -0,0 +1,152 @@
+----------------------------------------------------------------------------------
+-- Dawid Walczak
+-- 255643
+-- 15.05.2022r
+-- PROJEKT WSC
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use std.textio.all;
+use IEEE.STD_LOGIC_textio.all;
+USE ieee.numeric_std.ALL;
+entity dystrybutor is
+
+	port(
+		  clk : in std_logic;
+		  clk_pompa: in std_logic;
+		  reset : in std_logic; 
+		  
+		  czuj_paliwa: in std_logic_vector(7 downto 0):="00000000";
+		  paliwo: in std_logic_vector(1 downto 0):="00"; -- "01" - LPG; "10"- benzyna; "11" Diesel; "00" nie wybrano;
+		  koszty: out std_logic_vector(7 downto 0); -- wyjœcie z którego bêdê odczytywa³ za ile zatankowa³em i zapisywa³ tê liczbê do pliku
+		  litry: out std_logic_vector(7 downto 0); -- wyjœcie z którego bêdê odczytywa³ ile litrów zatankowa³em i zapisywa³ tê liczbê do pliku
+		  postep		: out std_logic; -- tankowanie skonczone
+		  
+		  --verilog
+		  data_out: out std_logic_vector(1 downto 0); 
+		  licznik_koszty1 :out std_logic_vector(7 downto 0);
+		  clockEn1: out std_logic:='0';
+		  licznik_litry1	: out std_logic_vector(7 downto 0)
+		  );  
+
+end entity dystrybutor;
+
+architecture dystrybutor_arch of dystrybutor is
+
+	type STANY is (czuwanie, rodz_paliwa , tankowanie , wydruk); -- fsm dystrybutora
+	signal stan, stan_nast: STANY; 
+	signal licznik_litry: std_logic_vector(7 downto 0); -- sygnal licznika zliczajacego ilosc zatankowanego paliwa
+	signal licznik_koszty: std_logic_vector(7 downto 0); -- sygnal licznika zliczajacego kwote tankowania
+	signal postep1: std_logic;
+	signal clockEn: std_logic;
+	
+begin
+	
+	reg:process(clk,reset) 
+		begin 
+			if (reset='0') then
+				stan <= czuwanie;
+			elsif (clk'event and clk='1') then
+				stan<=stan_nast;
+			end if;
+		end process reg;
+		
+	komb_proc:process (stan,paliwo,reset,postep1,czuj_paliwa,licznik_litry) 
+		begin
+			stan_nast<=stan;
+			
+			case stan is
+				when czuwanie =>
+					if (reset='1') then 
+						stan_nast<= rodz_paliwa;
+						
+					end if;
+					
+				when rodz_paliwa=> 
+					if (reset='1') then
+							if (paliwo /= "00") then 
+								stan_nast<=tankowanie;
+								
+							else
+								stan_nast<= rodz_paliwa; -- nie podano odpowiedniego paliwa
+								
+							end if;
+					else
+						stan_nast<=czuwanie;
+						
+					end if;
+					
+				when tankowanie => 
+					if (reset='1') then
+						if (licznik_litry=czuj_paliwa) then
+							stan_nast<=wydruk;
+							clockEn<='0';
+							
+						elsif (licznik_litry/=czuj_paliwa) then
+							stan_nast<=tankowanie;
+							clockEn<='1';
+							
+						end if;
+					else
+						stan_nast<=czuwanie;
+						
+					end if;
+				
+				when wydruk=>
+					if (postep1='0') then
+						stan_nast<=czuwanie;
+						
+					end if;	
+				
+		end case;
+	end process komb_proc;
+	
+	licznik_proc:process(clk,reset,stan,paliwo,clockEn,czuj_paliwa,clk_pompa) -- zliczanie ilosc paliwa oraz kosztow
+		begin
+			if (reset='0') then 
+				licznik_koszty<= (others => '0');
+				licznik_litry<= (others => '0');
+			elsif (clk_pompa'event and clk_pompa='1') then -- 1 takt zegara tankuje 1 litr paliwa i dolicza odpowiednia kwote za wybrany rodzaj paliwa
+					if (stan=tankowanie and clockEn='1') then
+						if (paliwo="10") then -- benzyna - 4 zl/litr 
+							licznik_litry<=licznik_litry+1;
+							licznik_koszty<=licznik_koszty+4;
+						
+							
+						elsif (paliwo="01") then -- LPG - 2 zl/litr
+							licznik_litry<=licznik_litry+1;
+							licznik_koszty<=licznik_koszty+2;
+							
+							
+						elsif (paliwo="11") then --diesel - 6 zl/litr
+							licznik_litry<=licznik_litry+1;
+							licznik_koszty<=licznik_koszty+6;
+							
+						else
+							licznik_litry <="00000000"; 
+							licznik_koszty<="00000000"; 
+					
+						end if;
+					else
+						licznik_litry<=(others=>'0');   
+						licznik_koszty<=(others=>'0');
+						
+					end if;
+			end if;
+		end process licznik_proc;
+			
+		postep1 <= '1' when czuj_paliwa=licznik_litry and stan=wydruk else '0';
+		litry <= licznik_litry when postep1='1' else "00000000";
+		postep<=postep1 when postep1='1' else '0';
+		koszty <= licznik_koszty when postep1='1' else "00000000" ;  
+		licznik_litry1<=licznik_litry;
+		licznik_koszty1<=licznik_koszty;
+		data_out<="00" when stan=czuwanie	
+		else "01" when stan=rodz_paliwa
+		else "10" when stan=tankowanie
+		else "11" when stan=wydruk;
+		clockEn1<='1' when stan=tankowanie else '0';
+					
+end dystrybutor_arch;
+
---
+++ test_ver.v
@@ -0,0 +1,216 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   15:21:52 05/09/2022
+// Design Name:   dystrybutor
+// Module Name:   D:/wsc_dys/dystrybutor_test.v
+// Project Name:  wsc_dys
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: dystrybutor
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module dystrybutor_test;
+
+	// Inputs
+	reg clk;
+	reg reset;
+	reg [1:0] paliwo;
+	reg clk_pompa;
+	// Outputs
+	reg [7:0] czuj_paliwa;
+	wire [7:0] koszty;
+	wire [7:0] licznik_koszty1;
+	wire [7:0] licznik_litry1;
+	wire [7:0] litry;
+	wire clockEn1;
+	wire [1:0] data_out;
+	
+	wire postep;
+
+	// Instantiate the Unit Under Test (UUT)
+	dystrybutor uut (
+		.clk(clk), 
+		.reset(reset), 
+		.paliwo(paliwo), 
+		.koszty(koszty),
+		.data_out(data_out),
+		.licznik_litry1(licznik_litry1),
+		.licznik_koszty1(licznik_koszty1),
+		.czuj_paliwa(czuj_paliwa),
+		.clk_pompa(clk_pompa),
+		.litry(litry), 
+		.clockEn1(clockEn1),
+		.postep(postep)
+	);
+
+		
+		
+always @ (data_out,paliwo,litry,postep,czuj_paliwa)
+begin
+
+		if (data_out==4'b0011)
+		begin
+			if (postep==1'b1)
+			begin
+				case(paliwo)
+				2'b01:
+						if(litry==czuj_paliwa)
+						begin
+							$display("licznik dla LPG dziala dobrze");
+						end
+						else
+							begin
+							$display("licznik dla LPG nie dziala dobrze");
+						end
+				2'b10:
+					if(litry==czuj_paliwa)
+						begin
+							$display("licznik dla benzyny dziala dobrze");
+						end
+					else
+						begin
+							$display("licznik dla benzyny nie dziala dobrze");
+						end
+				2'b11:
+					if(litry==czuj_paliwa)
+						begin
+							$display("licznik dla diesla dziala dobrze");
+						end
+					else
+					begin
+						$display("licznik diesla nie dziala dobrze");
+					end
+			  default : $display("nie podano paliwa");
+			endcase
+			end
+		end
+	end
+
+always @ (data_out,reset)
+begin
+
+			if (reset==1'b1)
+			begin
+				case(data_out)
+				2'b00:
+						if(2'b00)
+						begin
+							$display("Stan Czuwanie - nie dziala");
+						end
+						else
+							begin
+							$display("Stan czuwanie - dziala");
+						end
+				2'b01:
+					if(2'b01)
+						begin
+							$display("Stan rodz_paliwa - dziala");
+						end
+					else
+						begin
+							$display("Stan rodz_paliwa - nie dziala");
+						end
+				2'b10:
+					if(2'b10)
+						begin
+							$display("Stan tankowanie dzia³a");
+						end
+					else
+					begin
+						$display("Stan tankowanie nie dziala");
+					end
+				2'b11:
+					if(2'b11)
+						begin
+							$display("Stan wydruk dzia³a");
+						end
+					else
+					begin
+						$display("Stan wydruk nie dziala");
+					end
+			  default : $display("Wylaczenie maszyny");
+			endcase
+			end
+	end
+
+
+integer file;
+	initial begin
+		// Initialize Inputds
+		clk = 0;
+	
+		reset = 0;
+		forever
+			begin
+			#10 clk=~clk;	
+			end
+	end;
+	
+	always //@(posedge clk)
+	begin
+		if(!clockEn1)
+			begin
+				#5 clk_pompa=0;	
+			end
+		else 
+			begin
+			#30 clk_pompa=1;
+			#30 clk_pompa=0;
+			end
+	end
+	
+	
+		initial begin
+		file=$fopen("plik.txt","w");
+		$fmonitor(file, "reset|paliwo||licznik_litry1,licznik_koszty1|postep|fsm_state");
+		forever #10 $fmonitor(file, " |%b|   |%b|      |%d|         |%d|         |%b|       |%b|   ",reset,paliwo,licznik_litry1,licznik_koszty1,postep,data_out);
+	end;
+                      
+		initial begin
+		
+		reset=0;
+		#20;
+		reset=1;
+		paliwo=2'b00;
+		czuj_paliwa=7'b00010110;
+		paliwo = 2'b11;
+		
+		#1340;
+		
+		paliwo=2'b00;
+		#40;
+		
+		czuj_paliwa=7'b00011111;
+		paliwo = 2'b01;
+		#960;
+		
+		reset=0;
+		czuj_paliwa=7'b00000000;
+		paliwo=2'b00;
+		//paliwo = 2'b01;
+		//#920;
+		
+		//paliwo = 2'b00;
+		//#40
+		
+	//	reset=0;
+	//	paliwo =2'b00;
+		$finish;
+
+		
+      end
+endmodule
+
